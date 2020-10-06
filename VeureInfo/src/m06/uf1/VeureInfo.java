d38810e109b284572bf593e905c354b7b4459d9f
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-10-06 19:20:59 +0200

    final

--- VeureInfo/src/m06/uf1/VeureInfo.java
+++ VeureInfo/src/m06/uf1/VeureInfo.java
@@ -7,12 +7,18 @@ import java.util.Date;
 public class VeureInfo {
 
 	public static void main(String[] args) {
-		//Exercici 1
+		/*Exercici 1
 				//Fes un programa que s'anomeni VeureInfo que:
 
 			  //Accepti de línia d'ordres (args[0]) el directori o fitxer que es vol consultar. Cal comprova que existeix file.exists()
 			  // Si és un directori (file.isDirectory()) que mostri el seu contingut, com ho fa a CODI1.1
 				// Si és un fitxer (file.isFile()) que mostri la seva informació, com ho fa a CODI1.2
+		
+		NO-DUAL
+
+	    En el cas d'objectes ocults, indicar d'alguna manera en la sortida que es tracta efectivament d'un objecte ocult.
+	    S'ha d'indicar quina és l'última data que s'ha modificat l'objecte i, en el cas d'objectes modificats els darrers tres dies, s'ha d'indicar
+	    d'alguna manera aquest fet de manera VISIBLE.*/
 				
 				System.out.println("indique el fichero a consultar : ");
 				
@@ -22,7 +28,7 @@ public class VeureInfo {
 				String pattern = "yyyy-MM-dd hh:mm aa";  // Expresion regular que nos ayudará a verificar la fecha en un formato convencional, modificando el mètodo "lastModified" para 
 				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern); // clase SimpleDateFormat para ordenar los milisegundos que nos devuelve el método "lastModified" al formato que le indicamos en el pattern
 				
-				Date modificacion = new Date(lastModified );
+				Date modificacion = new Date(lastModified ); 
 				System.out.println( "El archivo " + comprobar + " fue modificado por última vez :  " + simpleDateFormat.format( lastModified ) );
 				
 				boolean Respuesta; // comprobacion lógica para el programa
--- VeureInfo/src/m3/uf4/Actividad01PowerOfNumber.java
+++ VeureInfo/src/m3/uf4/Actividad01PowerOfNumber.java
@@ -43,3 +43,4 @@ public class Actividad01PowerOfNumber {
 	}
 
 }
+
