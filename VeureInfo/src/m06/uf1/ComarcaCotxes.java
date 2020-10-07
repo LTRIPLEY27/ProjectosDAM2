c0484c8491cfea4b9542dbb395948fecc00f9c06
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-10-07 11:38:41 +0200

    init fichero

--- VeureInfo/src/m06/uf1/ComarcaCotxes.java
+++ VeureInfo/src/m06/uf1/ComarcaCotxes.java
@@ -1,7 +1,19 @@
 package m06.uf1;
 
-public class ComarcaCotxes {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ComarcaCotxes implements java.io.Serializable{ // la interfaz "seriarizable" indica que la clase hara uso de flujos de entrada y salida
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String marca;  // los atributos se han de definir en la clase al momento siguiente
 	private String modelo;
 	private int any;
@@ -27,36 +39,90 @@ S'ha d'entregar PDF amb el següent contingut:
 
 				
 		// primer objeto de la clase cotxes
-			
-	public String getMarca() { // método para mostrar el valor de un atributo
-			return marca;
-			}
-	public void setNombre (String marca) {
-		this.marca = marca; // método set (indica la modificacion potencial de un atributo)
-	}
+	
+	public ComarcaCotxes (String marca, String modelo, int any, int matricula) {
 		
-	public String getModelo() {
-			return modelo;
-	}
-	public void setModelo (String modelo) { // es imperativo declarar el parametroa modificar dentro del método para que compile
+		this.marca = marca;
 		this.modelo = modelo;
-	}
-	
-	public int getAny() {
-		return any;
-	}
-	public void setAny( int any) {
 		this.any = any;
-	}
-	
-	public int getMatricula() {
-		return matricula;
-	}
-	public void setMatricula (int matricula) {
 		this.matricula = matricula;
 	}
+	public ComarcaCotxes(String marca2, String modelo2, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getMarca() { // método para mostrar el valor de un atributo
+			return this.marca;
+			}
+	//public void setNombre (String marca) {
+		//this.marca = marca; // método set (indica la modificacion potencial de un atributo)
+	//}
+		
+	public String getModelo() {
+			return this.modelo;
+	}
+	//public void setModelo (String modelo) { // es imperativo declarar el parametroa modificar dentro del método para que compile
+		//this.modelo = modelo;
+	//}
 	
+	public int getAny() {
+		return this.any;
+	}
+	//public void setAny( int any) {
+		//this.any = any;
+	//}
 	
+	public int getMatricula() {
+		return this.matricula;
+	}
+	//public void setMatricula (int matricula) {
+		//this.matricula = matricula;
+	//}
+	
+	public String getDatosCotxes () {
+		return this.getMarca() + " " + this.getModelo() + " " + this.getAny() + " " + this.getMatricula();		
+	}
+	
+	/*public Constructor () {
+		File cotxes = new File ("cotxescomarca.txt");
+		Scanner scan = new Scanner ( cotxes );
+		
+		close()
+	}*/
+	
+	String cotxesFichero = "e:\\Objetos.dat"; {
+	try {
+		FileOutputStream ficheroExit = new FileOutputStream(cotxesFichero);
+		ObjectOutputStream objetoExit = new ObjectOutputStream(ficheroExit);
+		objetoExit.writeObject(new ComarcaCotxes ("ford" , "focus", "2016", "56879"));
+		objetoExit.close();
+	} catch (FileNotFoundException e) {
+		System.out.println("no encontrado ");
+	}  catch (IOException e) {
+		System.out.println(e.getMessage());
+	} catch (Exception e) {
+		System.out.println(e.getMessage());
+	};
 
+	try {
+		FileInputStream ficheroIn = new FileInputStream (cotxesFichero);
+		ObjectInputStream objetoIn = new ObjectInputStream (ficheroIn);
+		
+		ComarcaCotxes c1 = (ComarcaCotxes) objetoIn.readObject();
+		
+		objetoIn.close();
+		
+		System.out.println("Marca");
+		System.out.println(c1.getMarca());
+	}
+		catch (FileNotFoundException e) {
+			System.out.println(" el fichero no existe ");
+		}
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		};
+}
 }
 
--- VeureInfo/src/m09/uf1/CifradoCesarActividad01.java
+++ VeureInfo/src/m09/uf1/CifradoCesarActividad01.java
@@ -39,6 +39,7 @@ Feu un tercer programa que descodifiqui entrades utilitzant la «força bruta». El
 		
 		
 		
+		
 		//char [] alfabeto  = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'} ;
 		String[] alfabeto  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"} ;
 		
