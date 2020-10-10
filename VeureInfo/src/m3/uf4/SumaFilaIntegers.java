b06b58e3e59f0a63135fe8dcc8b6b1783a084af0
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-10-10 15:09:21 +0200

    final

--- VeureInfo/src/m3/uf4/SumaFilaIntegers.java
+++ VeureInfo/src/m3/uf4/SumaFilaIntegers.java
@@ -1,10 +1,6 @@
 package m3.uf4;
 
-import java.io.File;
 import java.io.*;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class SumaFilaIntegers {
@@ -12,110 +8,59 @@ public class SumaFilaIntegers {
 	public static void main(String[] args) throws IOException  {
 /*CFGS DAM/DAWM03-UF4-NF4-Activitat1-SumFileIntegers Write a program that adds all the integers in a file of text integers. This program will be similar to the ManySquaresexample program 
  * of the chapter and will use a similar input file. Prompt the user for the name of the input file.
- 
- 
-		
-		
-		Scanner teclat = new Scanner (System.in);
-		
-		int suma =0;
-		int n;
-		int [] fichero; // array para guardar los integers a almacenar
-		
-		File integer = new File ("e:\\integers.txt"); 
-		//Scanner Scan = new Scanner( "e:\\integers.txt" );
-		PrintWriter ficheroSuma = new PrintWriter ("e:\\integers.txt"); // clase PrintWriter para escribir sobre el fichero
-			
-		//output.println("esto es una prueba");
-		//output.close();
-		
-		System.out.println(" cuántos números desea almacenar en el fichero");
-		n = teclat.nextInt(); // dimensión a escoger del fichero
-		
-		fichero  = new int [n]; // adjunto el valor de n como dimensión del fichero
-		
-		
-			for (int i=0; i<fichero.length; i++) {
-				Scanner sc = new Scanner(System.in);  // adhesión del método scanner para delimitar al uso de Integers
-				System.out.print("Ingrese numeros : ");  // verificacion del Integer
-				int num = sc.nextInt();
-				  // si es "true"
-					//while (sc.hasNext()) { 
-						//System.out.println(sc.next());
-						ficheroSuma.write(fichero[i]); // carga en el array fichero
-			}
-			ficheroSuma.close();
-						//suma = suma + num;	
-							
-			
-					
-			}
-
-	
-	
-		//	System.out.println("La suma de los numeros es : " + suma );	
 */
 	Scanner teclat = new Scanner (System.in);	
-	int n;
+	int n; // variable a guardar con la instruccion de la cantidad de números a sumar
+	int [] fichero;  // arreglo del fichero para recorrer y almacenar los numeros con el parametro anterior
+	int suma=0;
+	int number;  // variable auxiliar
+	 
+	File num = new File ("e:\\integers.txt"); // creacion del fichero
+	FileWriter exit = new FileWriter (num);  // el método FileWriter funciona como escritura en el fichero, por elllo se le da el valor del (num)
+	PrintWriter output = new PrintWriter ("e:\\integers.txt");// método PrintWriter imprime los datos "escritos" en el método FileWriter
+	//FileReader lector = new FileReader (num);// método FileReader funciona para "Leer" los valores previamente "escritos" e "impresos, siempre se ha de seguir el orden
 	
-	//Suma suma;
-	File num = new File ("e:\\integers.txt");
-	FileInputStream exit = new FileInputStream (num);
-	DataOutputStream entrada = new DataOutputStream (exit);
-	
-	//FileOutputStream fichero1 = new FileOutputStream (num);
-	//ObjectOutputStream salida =  new ObjectOutputStream (fichero1);
+
 	
 	System.out.println("ingrese la cantidad de numeros a sumar : ");
-	n = teclat.nextInt();
+	n = teclat.nextInt(); // cantidad de numeros a almacenar y sumar
 	
-	int [] fichero = null;
+	fichero = new int [n]; // cantidad de numeros a guardar en el fichero (dimensión)
 	
 	for (int i=0; i<n; i++) {
-		entrada.writeInt(fichero[i]);
+		System.out.println("ingrese números por teclado ");
+		fichero [i] = teclat.nextInt(); // hacemos uso de la variable auxiliar a almacenar ya que aún no se pueden conpiar directamente en el fichero
+		suma = suma + fichero[i];
+	
+		
 	}
 	
-	entrada.close();
+	
+	for (int i=0; i<n; i++) {
+		exit.write(fichero[i]); // copiamos los valores del arreglo "fichero" en Fichero que creamos con el método "Write"
+	}
+	
+	exit.close(); // es imperativo cerrar cada método
+	
+	for (int i=0; i<n; i++) {
+		output.print(fichero[i]);
+	}
+	
+	output.close();
+		
+
+	try (Scanner scan = new Scanner (num)){// la sentencia funciona indicándoles la condición del bloque antes del paréntesis, puede ser más funcional
+			while (scan.hasNext()) {
+				number = scan.nextInt();				
+			}
+		System.out.println("la suma total es : " + suma);
+		
+		}catch (FileNotFoundException e) {
+		System.out.println();
+	}catch (IOException e) {
+		System.out.println();
+		}
 	}
 	
 }
-	//try {
-		
 	
-		
-	//} catch (Exception ex) {ex.printStackTrace();
-	
-	
-	
-	//// variable auxiliar donde se almacena el fichero a leer
-	//Integer n;
-	
-	
-	
-	//String escribir = "escribir este mensaje";  // variable a almacenar en el fichero de prueba
-	//String leido = " "; // variable auxiliar donde se almacenara
-	
-	//blank = fichero;
-	
-	//fichero.toString();
-	/*
-	try {  // siempre se ha de usar el método try para controlar el flujo de potenciales errores pues al leer un archivo podrian ocurrir
-		FileWriter integers = new FileWriter ("e:\\integers.txt"); //creacion de la ruta del fichero
-		integers.write(escribir); // le indicamos que al fichero ha de escribirse lo que se definio en la variable String que queriamos guardar
-		integers.close(); // es imperativo que se cierre con (close()) pues en caso contrario no almacena nada
-		
-	} catch (Exception ex) { ex.printStackTrace(); }// manejo del flujo de error  IMPORTANTÍSIMO
-			
-	try { // se han de indicar excepciones a comprobar el flujo por cada dato (uno para escribir, otro para leer y así dependiendo de lo que deseemos)
-		FileReader lector = new FileReader ("e:\\integers.txt"); // se le indice el fichero a leer
-		BufferedReader leer = new BufferedReader (lector);  // BufferedReader es una variable necesaria en la lectura de ficheros, ya que almacena la lectura que se aya haciendo del mismo
-		leido = leer.readLine(); 
-		
-	} catch (Exception ex) { ex.printStackTrace();}
-	
-	System.out.println (" se ha guardado " + leido);*/
-	
-
-
-
-
