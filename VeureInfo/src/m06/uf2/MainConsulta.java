bce510a5e0c8992290f944aa402777035aaf26aa
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-11-28 16:35:51 +0100

    0

--- VeureInfo/src/m06/uf2/MainConsulta.java
+++ VeureInfo/src/m06/uf2/MainConsulta.java
@@ -12,15 +12,68 @@ public class MainConsulta {
 		
 		boolean respuesta = false;
 		
-		int agno, postal;
+		int agno, postal, opcion;
 		
 		MySQLFirts tabla = new MySQLFirts (null, null, 0, null, null, 0, null);
 		
 		System.out.println("la tabla sobre la que se trabaja es la siguiente");
 		tabla.mostrarTabla();
 		
-		while (respuesta) {
+		System.out.println("ingrese la opcion de su preferencia: " + '\n' + 
+				"desea insertar alumno (1)" + '\n' + " desea modificar alumno (2)" + '\n' +
+				"desea eliminar alumno  (3)" + '\n' + "modificar poblacion (4)" + '\n' + "salir (5)");
+		
+		opcion = ask.nextInt();
+		
+		while (opcion < 5) {
+			switch (opcion) {
 			
+			case 1:
+				System.out.println(" ingrese nombre");
+				name = ask.next();
+				
+				System.out.println(" ingrese DNI");
+				ident = ask.next();
+				
+				System.out.println(" ingrese any de nacimiento");
+				agno = ask.nextInt();
+				
+				System.out.println(" ingrese direccion postal");
+				direct = ask.next();
+				
+				System.out.println(" ingrese sexo");
+				sex = ask.next();
+				
+				System.out.println(" ingrese código postal");
+				postal = ask.nextInt();
+				
+				System.out.println(" ingrese poblacion");
+				pobla = ask.next();
+
+				
+				MySQLFirts nueva = new MySQLFirts (name , ident, agno, direct, sex, postal, pobla);
+			
+				nueva.metodoInsertar();
+				nueva.mostrarTabla();
+				break;
+				
+			case 2:
+				//String modifica = ("UPDATE  ");
+				String eleccion;
+				int valor;
+				String nuevoValor;
+				System.out.println(" ingrese la columna que desea modificar");
+				eleccion = ask.next();
+				System.out.println(" ingrese el numero de la fila que desea modificar  ");
+				valor = ask.nextInt();
+				System.out.println(" ingrese el nuevo valor a modificar");
+				nuevoValor = ask.next();
+				String modifica = ("UPDATE ALUMNE SET " + eleccion + " = " + nuevoValor + "WHERE " + eleccion + " = " + valor);
+				//if (eleccion.equalsIgnoreCase(name)) {
+				tabla.mostrarTabla();	
+				//}
+				break;
+			}
 			
 		}
 		/*
