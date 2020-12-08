e5b83a585bce0676ca7df3bf1c4aec168e888d5f
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-12-08 19:50:30 +0100

    3

--- VeureInfo/src/m3/uf5Actividad3/CoffeTester.java
+++ VeureInfo/src/m3/uf5Actividad3/CoffeTester.java
@@ -2,4 +2,41 @@ package m3.uf5Actividad3;
 
 public class CoffeTester {
 
+	// atributo de la clase
+	private double valor;
+	
+
+	// constructor 
+	public CoffeTester(double num) {
+		
+		this.valor = num;
+	}
+	
+	// método set
+	public void setValor (double num) {
+		this.valor = num;
+	}
+	
+	// método Get
+	public double getValor () {
+		
+		return this.valor;
+	}
+
+	@Override
+	public String toString() {
+		return "CoffeTester [valor=" + valor + "]";
+	}
+	
+	public static int findMax ( int[] x) {
+		
+		int max = x [0]; 
+		for ( int i = 0; i < x.length; i ++ ) {
+			if (x [i] < max )
+				max = x[i];
+			
+			return max;
+		}
+		return max;
+	}
 }
--- VeureInfo/src/m3/uf5Actividad3/MainCoffeTester.java
+++ VeureInfo/src/m3/uf5Actividad3/MainCoffeTester.java
@@ -1,10 +1,39 @@
 package m3.uf5Actividad3;
 
+import java.util.Scanner;
+
+/*
+ * Alumne: María Isabel Calzadilla.
+ * DAM UF5
+ * Date: 08-12-2020
+ * Activity: Actividad 03 m03UF5
+ */
 public class MainCoffeTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		double num;
+		Scanner ask = new Scanner (System.in);
+		//15
+		CoffeTester [] arrayCoffe = new CoffeTester[3];
+		
+		// carga de datos manual del array
+		for (int i = 0; i < arrayCoffe.length; i++) {
+			System.out.println(" ingrese los valores de PH a almacenar");
+			num = ask.nextDouble();
+			
+			arrayCoffe[i] = new CoffeTester (num);
+		}
+		
+		// impresion del array mediante un toString
+		for (int i = 0; i < arrayCoffe.length; i ++) {
+			System.out.println(arrayCoffe[i].toString());
+		}
+		
+		double maxValor = CoffeTester.findMax();
+		
+		System.out.println(maxValor);
+	
 
 }
