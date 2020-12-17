3f86a0a9cde2a42a40df7679ae1db5568c93e6a1
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2020-12-17 21:41:32 +0100

    3

---
+++ VeureInfo/src/m3/uf5Actividad7/EmptySudokuTester.java
@@ -0,0 +1,13 @@
+package m3.uf5Actividad7;
+
+public class EmptySudokuTester {
+	// atributos
+	private char [][]  sudoku;
+	
+	
+	public EmptySudokuTester () {
+		
+		this.sudoku = 
+	}
+	
+}
---
+++ VeureInfo/src/m3/uf5Actividad7/MainSudokuTester.java
@@ -0,0 +1,11 @@
+package m3.uf5Actividad7;
+
+public class MainSudokuTester {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
