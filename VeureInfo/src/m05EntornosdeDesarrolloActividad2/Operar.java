51e0d5fc21036aff087c1363348201db87a35513
isabellbaixcamp <71727244+isabellbaixcamp@users.noreply.github.com>
2021-03-07 21:51:43 +0100

    final

--- VeureInfo/src/m05EntornosdeDesarrolloActividad2/Operar.java
+++ VeureInfo/src/m05EntornosdeDesarrolloActividad2/Operar.java
@@ -4,7 +4,7 @@ package m05EntornosdeDesarrolloActividad2;
 public class Operar {
 
 	private double saldo;
-	private double y;
+	//private double y;
 	
 	public Operar() {
 		this.saldo = 0.0;  // saldo inicial 
@@ -14,26 +14,70 @@ public class Operar {
 		return this.saldo;
 	}
 	
-	public boolean getSaldoTester(double x) {
+	
+	public boolean getSaldoTester(double x) {  // ********metodo anidado a verificar en el saldo a actualizar *********
 		if(x < 0) {
-			return false;       // boolean que verifica que la cantidad cumpla 3 condiciones: 1) sea numero positivo(x>0). 2) sea menor o igual a 6000. 3) sea un numero de dos decimales 
+			return true;       // boolean que verifica que la cantidad cumpla 3 condiciones: 1) sea numero positivo(x>0). 2) sea menor o igual a 6000. 3) sea un numero de dos decimales 
 		} if(x > 6000) {
-			return false;
-		} if(Math.round(x * 100.0)/100.0 == x * 100.0) { // condicioneal a verificar = si lleva dos decimales comparando con el mismo parametro 
-			return false;
-		}
-		
-		return true;
+			return true;
+		} if(Math.round(x * 100) != x * 100) { // condicioneal a verificar = si lleva dos decimales comparando con el mismo parametro 
+			return true;		
+		} 
+		return false;  // condicional: no es menor a 0, no es mayor a 6000 y no tiene mas de 2 decimales
 	}
 	
-	public void actualizaSaldo(double x) {
-		if(getSaldoTester(x)== true) {
-			y += x;
+	public void actualizaSaldo(double x) {   // metodo anidado del getSaldoTester, reduce el codigo a validar las 3 condiciones esenciales
+		if(getSaldoTester(x) != true) {
+			this.saldo += x;
 		}
 	}
 	
 	public double getActualizaSaldo() {
-		return y;
+		return this.saldo;
+	}
+	
+	//////////////////////// RETIRO ////////////////////
+	
+	public boolean setSaldoTesterRetiro(double x) {
+		if(x > this.saldo) {       // x es menor al saldo
+			return true;
+		} if(x < 0) {
+			return true;   // x es mayor a cero o numero positivo
+		} if(Math.round(x * 100) != x * 100) {   // x debe de tener 2 decimales
+			return true;
+		} if(x > 6000) {   // x debe de ser menos a 6000
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void setActualizaSaldoRetirada(double x) {
+		if(setSaldoTesterRetiro(x) == false) {
+			this.saldo -= x;   // resta x a saldo
+		}
+	}
+	
+	public boolean setTransfiereEntreCuentas(double x, double y, double z) {
+		
+		if(y > x) {
+			return true;
+		} if((y < 0) || (x < 0) || (z < 0)) {
+			return true;
+		} if((Math.round(y * 100) != y * 100) || (Math.round(y * 100) != y * 100) || ((Math.round(z * 100) != z * 100))) {	
+			return true;
+		} if(z > 3000) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public void setActualizaTransferenciaSaldo(double x, double y, double z) {
+		if(setTransfiereEntreCuentas(x, y, z) == false) {
+			this.saldo = y + z;
+		}
 	}
 	
 }
